9d26fc2edbe1eaa66a4308d85c4db445e6cf46b4
josh <aaronbaon1@gmail.com>
2024-11-30 23:01:24 +0800

    add more styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
-import Nav from "./components/nav";
+import Nav from "./components/Nav";
 
 function App() {
   return <Nav />;
+
 }
 
 export default App;
--- frontend/src/components/nav.tsx
+++ frontend/src/components/nav.tsx
@@ -2,19 +2,19 @@ import { CgProfile } from "react-icons/cg";
 
 function Nav() {
   return (
-    <nav className="space-x-96 mx-auto flex w-full items-center justify-between p-6 lg:px-8">
-      <h3 className="mr-auto">Shironzi</h3>
-      <div className="flex space-x-4 mx-auto">
-        <a href="">Laptops</a>
-        <a href="">Desktop PCs</a>
-        <a href="">Networking Devices</a>
-        <a href="">Printers & Scanners</a>
-        <a href="">PC Parts</a>
-        <a href="">All Other Products</a>
-        <a href="">Repairs</a>
-        <a href="">Our Deals</a>
+    <nav className="flex justify-between px-40 py-5 items-center bg-gradient-to-r">
+      <a href=""><h1 className="font-explore text-3xl font-semibold">Shironzi</h1></a>
+      <div className="flex justify-between space-x-9 items-center">
+        <a className="font-medium" href="">Laptops</a>
+        <a className="font-medium" href="">Desktop PCs</a>
+        <a className="font-medium" href="">Networking Devices</a>
+        <a className="font-medium" href="">Printers & Scanners</a>
+        <a className="font-medium" href="">PC Parts</a>
+        <a className="font-medium" href="">All Other Products</a>
+        <a className="font-medium" href="">Repairs</a>
+        <a className="border-solid border-2 blue-border rounded-full px-6 py-1 blue font-medium " href="">Our Deals</a>
       </div>
-      <div className="ml-auto">
+      <div className="">
         <CgProfile size={"2.5rem"} />
       </div>
     </nav>
