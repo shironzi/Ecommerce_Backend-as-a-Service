426d5400cec34d5b3c5720a3c39525c37270fbd8
josh <aaronbaon1@gmail.com>
2024-11-30 21:27:46 +0800

    add Tailwind CSS and autoprefixer support

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{tsx,ts,html}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
